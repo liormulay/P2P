6dd55a57e82ba4f126a6dd55d858382b0b80de4d
liorm <liormualy6@gmail.com>
2021-01-07 21:42:36 +0200

    Initializes Bluetooth adapter.

--- app/src/main/java/com/example/p2p/MainActivity.java
+++ app/src/main/java/com/example/p2p/MainActivity.java
@@ -2,22 +2,30 @@ package com.example.p2p;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BluetoothAdapter bluetoothAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         hasBLE();
+        // Initializes Bluetooth adapter.
+        final BluetoothManager bluetoothManager = (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        bluetoothAdapter = bluetoothManager.getAdapter();
     }
 
     private void hasBLE() {
-    // Use this check to determine whether BLE is supported on the device. Then
-    // you can selectively disable BLE-related features.
+        // Use this check to determine whether BLE is supported on the device. Then
+        // you can selectively disable BLE-related features.
         if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
             Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
             finish();
