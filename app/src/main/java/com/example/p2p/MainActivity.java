20b83911daec72711b33c5fbd7130edf789479b9
liorm <liormualy6@gmail.com>
2021-01-07 21:31:28 +0200

    Initial commit

---
+++ app/src/main/java/com/example/p2p/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.p2p;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        hasBLE();
+    }
+
+    private void hasBLE() {
+    // Use this check to determine whether BLE is supported on the device. Then
+    // you can selectively disable BLE-related features.
+        if (!getPackageManager().hasSystemFeature(PackageManager.FEATURE_BLUETOOTH_LE)) {
+            Toast.makeText(this, R.string.ble_not_supported, Toast.LENGTH_SHORT).show();
+            finish();
+        }
+    }
+}
